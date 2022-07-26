02adf2cce5b74717ceec9ab066cc8439b2e96155
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2022-07-26 14:15:37 +0200

    More operations added 💻

--- api/utils.js
+++ api/utils.js
@@ -1,13 +1,16 @@
+const math = require("mathjs");
 const basicOps = {
   add: (a, b) => Number(a) + Number(b),
   subtract: (a, b) => Number(a) - Number(b),
   multiply: (a, b) => Number(a) * Number(b),
   divide: (a, b) => (b !== 0 ? Number(a) / Number(b) : "Cannot divide by 0"),
+  simplify: (a) => math.simplify(a).toString().split(" ").join("")
 };
 
 const advancedOps = {
-  root: (a) => Math.sqrt(a),
+  root: (a) => math.sqrt(a),
   pow: (a, b) => Math.pow(a, b),
+  log: (a) => math.log10(a),
   factorial: (a) => {
     if (a <= 0) return "Number must be greater than 0";
     let f = 1;
@@ -16,6 +19,8 @@ const advancedOps = {
     }
     return f;
   },
+  derivative: (f) => math.derivative(f,math.parse("x")).toString().split(" ").join("")
+  
 };
 const trigonometryOps = {
   cos: (a) => Math.cos(a),
@@ -41,7 +46,8 @@ const operate = (operation, expression) => {
     operation == "add" ||
     operation == "subtract" ||
     operation == "multiply" ||
-    operation == "divide"
+    operation == "divide" ||
+    operation == "simplify"
   ) {
     operationType = basicOps;
     const [a, b] = expression.split(sign);
@@ -49,7 +55,9 @@ const operate = (operation, expression) => {
   } else if (
     operation == "root" ||
     operation == "pow" ||
-    operation == "factorial"
+    operation == "factorial" ||
+    operation == "derivative" ||
+    operation == "log"
   ) {
     operationType = advancedOps;
     result = operationType[operation](expression);
